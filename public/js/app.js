f584eaf5211a8b1224870e99e72076772c7af1e7
rangle13 <rangle@theangles.com>
2020-04-27 21:58:58 -0400

    Change utils directory

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,18 @@
 console.log("Client side js loaded.");
-fetch('/weather?address=Boston').then((response) => {
-    response.json().then ((data) => {
-        if(data.error){
-            console.log(data.error);
-        }else{
-            console.log(data.forecast);
-        }
-    });
-});
 
 const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
 
-weatherForm.addEventListener('submit', () => {
-    alert('Testing')
-    console.log('testing!');
-});
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value;
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then ((data) => {
+            if(data.error){
+                console.log(data.error);
+            }else{
+                console.log(data.forecast);
+            }
+        });
+    });
+})
--- src/app.js
+++ src/app.js
@@ -20,55 +20,55 @@ app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
-    })
-})
+        name: 'Rod Angle'
+    });
+});
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
-    })
-})
+        name: 'Rod Angle'
+    });
+});
 
 app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
-    })
-})
+        name: 'Rod Angle'
+    });
+});
 
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
         return res.send({
             error: 'You must provide an address!'
-        })
+        });
     }
 
     geocode(req.query.address, (error, { latitude, longitude, location }) => {
         if (error) {
-            return res.send({ error })
+            return res.send({ error });
         }
 
         forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
-                return res.send({ error })
+                return res.send({ error });
             }
 
             res.send({
                 forecast: forecastData,
                 location,
                 address: req.query.address
-            })
-        })
-    })
-})
+            });
+        });
+    });
+});
 
 app.get('/products', (req, res) => {
     if (!req.query.search) {
